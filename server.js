1e6de5b19e967ff4f5e825c272e0a1fa44a4cd65
Tejas Kotekar <tejaskotekar04@gmail.com>
2025-08-19 18:09:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ import cors from 'cors';
 import morgan from 'morgan';
 import "express-async-errors";
 
+// API DOcumenATion
+import swaggerUi from "swagger-ui-express";
+import swaggerDoc from "swagger-jsdoc";
+
 //security packages
 import helmet from 'helmet';
 import xss from 'xss-clean';
@@ -28,6 +32,27 @@ dotenv.config();
 //MongoDB connection
 connectDB();
 
+// Swagger api config
+// swagger api options
+const options = {
+    definition: {
+        openapi: "3.0.0",
+        info: {
+            title: "Job Portal Application",
+            description: "Node Express.js Job Portal Application",
+        },
+        servers: [
+            {
+                url: "http://localhost:8080",
+                //url: "https://nodejs-job-portal-app.onrender.com"
+            },
+        ],
+    },
+    apis: ["./routes/*.js"],
+};
+
+const spec = swaggerDoc(options);
+
 
 //rest object
 const app = express();
@@ -46,6 +71,10 @@ app.use('/api/v1/test', testRoutes);
 app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/user', userRoutes);
 app.use('/api/v1/job', jobsRoute);
+
+
+//homeroute root
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(spec));
 
 
 //validaton middleware
