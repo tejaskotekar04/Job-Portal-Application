4b32691dfb96a830cfeaaef84de81ed286df6ffd
Tejas Kotekar <tejaskotekar04@gmail.com>
2025-08-19 18:10:23 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,9 +17,102 @@ const limiter = rateLimit({
 const router = express.Router();
 
 //routes
+//identation is important for swagger documentation
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - name
+ *        - lastName
+ *        - email
+ *        - password
+ *      properties:
+ *        id:
+ *          type: string
+ *          description: The Auto-generated id of user collection
+ *          example : GDHJGD788BJBJ
+ *        name:
+ *          type: string
+ *          description: User name
+ *        lastName:
+ *          type: string
+ *          description: User Last Name
+ *        email:
+ *          type: string
+ *          description: User email address
+ *        password:
+ *          type: string
+ *          description: User password should be minimum 6 characters
+ *        location:
+ *          type: string
+ *          description: User location city or country
+ *      example:
+ *        id: GDHJGD788BJBJ
+ *        name: John
+ *        lastName: Doe
+ *        email: johndoes@gmail.com
+ *        password: test@123
+ *        location: mumbai
+ */
+
+/**
+ *  @swagger
+ *  tags:
+ *    name: Auth
+ *    description: Authentication APIs
+ */
+
+/**
+ * @swagger
+ * /api/v1/auth/register:
+ *    post:
+ *      summary: Register a new user
+ *      tags: [Auth]
+ *      requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/User'
+ *      responses:
+ *        200:
+ *          description: User created successfully
+ *          content:
+ *            application/json:
+ *              schema:
+ *                $ref: '#/components/schemas/User'
+ *        500:
+ *          description: internal serevr error
+ */
 
 //REGISTER || POST
 router.post('/register', limiter, registerController);
+
+/**
+ * @swagger
+ * /api/v1/auth/login:
+ *  post:
+ *    summary: Login Page
+ *    tags: [Auth]
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/User'
+ *    responses:
+ *      200:
+ *        description: Login successfull
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *      500:
+ *        description: something went wrong
+ */
 
 //LOGIN || POST
 router.post('/login', limiter, loginController);
